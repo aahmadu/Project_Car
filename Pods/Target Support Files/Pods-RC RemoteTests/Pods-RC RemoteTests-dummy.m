eb00bd2ef0505287c2b438679651d1fec6cd4600
ahmed <ahmed@EwertCS06-2.local>
2016-12-12 11:02:17 +0000

    cleaned up

--- Pods/Target Support Files/Pods-RC RemoteTests/Pods-RC RemoteTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RC_RemoteTests : NSObject
-@end
-@implementation PodsDummy_Pods_RC_RemoteTests
-@end
--- Pods/Target Support Files/Pods-RC RemoteUITests/Pods-RC RemoteUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_RC_RemoteUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RC_RemoteUITestsVersionString[];
-
