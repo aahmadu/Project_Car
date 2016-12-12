42fdebf582bd9b246e377f00e2ea124049e6799d
Ahmed Ahmadu <Ahmed@Ahmeds-MacBook-Pro.local>
2016-12-12 11:04:48 +0000

    ios app

---
+++ iOS_P_RC/Pods/Target Support Files/Pods-iOS_P_RC/Pods-iOS_P_RC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_P_RC : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_P_RC
+@end
---
+++ iOS_P_RC/Pods/Target Support Files/Pods-iOS_P_RCTests/Pods-iOS_P_RCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_P_RCTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_P_RCTests
+@end
